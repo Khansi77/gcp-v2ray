ca8669954abd579786c568abfcdb42e374416ef8
Khansi77 <152176679+Khansi77@users.noreply.github.com>
2026-02-17 21:47:37 +0700

    Update gcp-cloud-run.sh

--- gcp-cloud-run.sh
+++ gcp-cloud-run.sh
@@ -390,7 +390,7 @@ printf "   ${C_404_CYAN}${BOLD}%s${RESET}\n\n" "${URL_CANONICAL}"
 
 # =================== VLESS Configuration ===================
 VLESS_UUID="ba0e3984-ccc9-48a3-8074-b2f507f41ce8"
-URI="vless://${VLESS_UUID}@vpn.googleapis.com:443?path=%2F%40nkka404&security=tls&encryption=none&host=${CANONICAL_HOST}&type=ws&sni=vpn.googleapis.com#CHANNEL-404-VLESS-WS"
+URI="vless://${VLESS_UUID}@vpn.googleapis.com:443?path=%2F%40ksnnet&security=tls&encryption=none&host=${CANONICAL_HOST}&type=ws&sni=vpn.googleapis.com#CHANNEL-404-VLESS-WS"
 
 printf "${C_404_GREEN}${BOLD}🔑 VLESS CONFIGURATION:${RESET}\n"
 printf "   ${C_404_CYAN}%s${RESET}\n\n" "${URI}"
@@ -399,7 +399,7 @@ printf "${C_404_GREEN}${BOLD}📋 CONFIGURATION DETAILS:${RESET}\n"
 show_kv "UUID:" "$VLESS_UUID"
 show_kv "Host:" "vpn.googleapis.com"
 show_kv "Port:" "443"
-show_kv "Path:" "/@nkka404"
+show_kv "Path:" "/@ksnnet"
 show_kv "Security:" "TLS"
 show_kv "Transport:" "WebSocket"
 show_kv "SNI:" "vpn.googleapis.com"
